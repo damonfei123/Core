c28fb9083057b7b66c706ec1ca8ad8f431905c63
damonfei <317030876@qq.com>
2015-05-11 23:42:33 +0800

    fix

--- src/Component/RDB/ORM/PDODecorator.php
+++ src/Component/RDB/ORM/PDODecorator.php
@@ -509,7 +509,6 @@ class PDODecorator {
 
     public function buildQuerySQL(&$aArgs)
     {
-        $this->aWhere = empty($this->aWhere) ? '1' : $this->aWhere;
         return sprintf('SELECT %s FROM %s %s %s WHERE %s %s %s %s %s',
             $this->sSelect ? $this->sSelect : '*',
             $this->getRealMapTable(),
